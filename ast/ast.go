fa11a96695366da27695b016ef325a59c864a086
Gam8it <codemonkeymtndew@gmail.com>
2024-04-19 22:16:27 -0600

    some prefix operators

--- ast/ast.go
+++ ast/ast.go
@@ -21,7 +21,7 @@ func (p *Program) TokenLiteral() string {
 func (p *Program) String() string {
 	var out bytes.Buffer
 
-	for _,s := range p.Statements {
+	for _, s := range p.Statements {
 		out.WriteString(s.String())
 	}
 	return out.String()
@@ -70,7 +70,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-func (i *Identifier) String() string { return i.Value}
+func (i *Identifier) String() string       { return i.Value }
 
 type ReturnStatement struct {
 	Token       token.Token
@@ -92,7 +92,6 @@ func (rs *ReturnStatement) String() string {
 	return out.String()
 }
 
-
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
@@ -106,3 +105,31 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string       { 
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+ }
